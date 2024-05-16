19bfd687bb4862c2754754b95574125fb3208302
diwakar kumar <developer.diwakar@gmail.com>
2024-05-16 21:23:53 +0530

    Fixed - Sum - RPC error reading from server

--- calculator/server/sum.go
+++ calculator/server/sum.go
@@ -7,7 +7,7 @@ import (
 	pb "github.com/developerdiwakar/grpc-go-course/calculator/proto"
 )
 
-func Sum(ctx context.Context, in *pb.SumRequest) (*pb.SumResponse, error) {
+func (*Server) Sum(ctx context.Context, in *pb.SumRequest) (*pb.SumResponse, error) {
 	log.Println("Sum function was invoked with: ", in)
 	sum := in.FirstNumber + in.SecondNumber
 	return &pb.SumResponse{
